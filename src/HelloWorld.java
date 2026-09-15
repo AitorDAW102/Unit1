a5678b9bf5273781388d221ba9996ec3fe9764a6
AitorDAW102 <daw102.2027@gmail.com>
2026-09-15 09:32:40 +0200

    variables int y boolean

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
     public static void main(String[] arguments) {
         String a= "Hello World!";
+        int num = 8;
         System.out.println(a);
+        System.out.println(num);
+        boolean apagado = false;
+        System.out.println(apagado);
     }
 }
