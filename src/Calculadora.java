3b4ceae60d1fd3dcef3b9b61093b5b40560e9987
AitorDAW102 <daw102.2027@gmail.com>
2026-09-16 11:36:50 +0200

    constantes y operaciones

---
+++ src/Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+    public static void main(String[] args){
+        double firstOperand= 55;
+        double secondOperand= 45;
+        double totalSum=(firstOperand+secondOperand);
+        System.out.println("El resultado de la suma es: "+totalSum);
+    }
+
+}
---
+++ src/Root.java
@@ -0,0 +1,7 @@
+public class Root {
+    public static void main(String[] args) {
+        double num=144;
+        double result = Math.sqrt(num);
+        System.out.println("El resultado de la raiz cuadrada es "+ result);
+    }
+}
